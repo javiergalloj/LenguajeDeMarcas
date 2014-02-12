b37286d33352880a58dfaeb543c6db680bc8bdbc
javiergalloj <javiergalloj@hotmail.com>
2014-02-12 14:43:21 +0100

    Creo la función orientacion.

--- apiweather_html.py
+++ apiweather_html.py
@@ -4,20 +4,25 @@ import json
 import requests
 import webbrowser
 
-provincias = ['Almería','Cádiz', 'Córdoba','Granada','Huelva','Jaen','Málaga','Sevilla']
+def function orientacion (direccion):
+	if direccion == 0:
+		return 'N'
+	if direccion > 0 and direccion < 90:
+		return 'NE'
+	if direccion == 90:
+		return 'E'
+	if direccion > 90 and direccion < 180:
+		return 'SE'
+	if direccion == 180:
+		return 'S'
+	if direccion > 180 and direccion < 270:
+		return 'SO'
+	if direccion == 270:
+		return 'O'
+	if direccion > 270 and direccion < 360:
+		return 'NO'
 
-html = open('tiempo.html','w')
-html.write("""<!DOCTYPE html PUBLIC '-// W3C // DTD XHTML 1.0 Strict //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
-<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='es' charset="utf-8">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta charset="UTF-8">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>EL TIEMPO</title>
-</head>
-<body>
-<h1>EL TIEMPO EN ANDALUCIA</h1>
-""")
+provincias = ['Almería','Cádiz', 'Córdoba','Granada','Huelva','Jaen','Málaga','Sevilla']
 
 for provincia in provincias:
 	html.write("<h2>%s</h2>" % provincia)
@@ -30,29 +35,6 @@ for provincia in provincias:
 	viento = pronostico_prov['wind']['speed']
 	direccion = pronostico_prov['wind']['deg']
 	direccion = int(direccion)
-	if direccion == 0:
-		direccion = 'N'
-	if direccion > 0 and direccion < 90:
-		direccion = 'NE'
-	if direccion == 90:
-		direccion = 'E'
-	if direccion > 90 and direccion < 180:
-		direccion = 'SE'
-	if direccion == 180:
-		direccion = 'S'
-	if direccion > 180 and direccion < 270:
-		direccion = 'SO'
-	if direccion == 270:
-		direccion = 'O'
-	if direccion > 270 and direccion < 360:
-		direccion = 'NO'
-	html.write('''<ul>
-		<li>Temperatura mínima prevista: %s ºC.</li>
-		<li>Temperatura máxima prevista: %s ºC.</li>
-		<li>Viento: %s Km/h. Dirección: %s.</li>
-		</ul>''' % (temp_min,temp_max,viento,direccion))
+	direccion = orientacion(direccion)
 
-html.write("</body>")
-html.write("</html>")
-html.close()
 webbrowser.open("tiempo.html")
